541fdeb77f89f27d506d51d4d78b8388d7c7904f
stiglicode <stiglic.231@gmail.com>
2021-04-23 17:09:55 +0200

    Initial class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("Aaaa");
+// const shortCutNumber = (number) => {
+//     const stringNumber = number.toString();
+//     if(number > 999 && number <= 9999) {
+//         if(stringNumber[1] >= 1) {
+//             return `${stringNumber.substr(0, 1)},${stringNumber.substr(1, 1)}k`
+//         }else {
+//             return `${stringNumber.substr(0, 1)}k`
+//         }
+//     } else if(number > 9999 && number <= 99999) {
+//         if(stringNumber[2] >= 1) {
+//             return `${stringNumber.substr(0, 2)},${stringNumber.substr(2, 1)}k`
+//         }
+//         else {
+//             return `${stringNumber.substr(0, 2)}k`
+//         }
+//     } else if(number > 99999 && number <= 999999) {
+//         if(stringNumber[3] >= 1) {
+//             return `${stringNumber.substr(0, 3)},${stringNumber.substr(3, 1)}k`
+//         }
+//         else {
+//             return `${stringNumber.substr(0, 3)}k`
+//         }
+//     } else if(number < 999) {
+//         return stringNumber;
+//     }
+// }
+class LongNumberShort {
+    constructor(){
+        this._longNumber;
+        this._numberSuffix = {
+            bilionSK: "mld",
+            bilionEN: "b",
+            milion: "m",
+            thousand: "k"
+        }
+    }
+
+}
